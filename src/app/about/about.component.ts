4c1bd799f09240f37c65306ad309b36373181747
Daniel Wilches <dwilches@gmail.com>
2021-04-24 20:13:24 -0500

    Small updates

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+    sections = [
+        {
+            name: "Languages",
+            class: "badge-primary",
+            items: ["C++", "Java", "PL/SQL", "C#", "TypeScript", "Erlang"]
+        },
+        {
+            name: "Databases",
+            class: "badge-info",
+            items: ["Oracle", "PostgreSQL", "MongoDB (NoSQL)"]
+        },
+        {
+            name: "Other",
+            class: "badge-primary",
+            items: ["AWS (EC2, RDS)", "REST WebServices", "HTML", "Node", "Angular", "Android", "Arduino"]
+        }
+    ];
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NavigationEnd, Router} from "@angular/router";
 
 declare var gtag;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
-  constructor(public router: Router) {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        gtag('event', event.urlAfterRedirects);
-      }
-    });
-  }
+    constructor(public router: Router) {
+        this.router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                gtag('event', event.urlAfterRedirects);
+            }
+        });
+    }
 
 }
